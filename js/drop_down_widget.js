a75830bba8ba9f1ec5c1b7b5645f19b3f6f2855e
yschong1759 <yschong1759@gmail.com>
2015-07-29 22:08:22 +0800

    inserted add and minus button, dropdown list not fixed yet

--- js/drop_down_widget.js
+++ js/drop_down_widget.js
@@ -1,27 +1,22 @@
 $(document).ready(function() {
   $('.add-new').click(function () {
     $(this).before('<div class="form-group selection no-margin">\
-        <label for="select" class="col-md-1 control-label"><i class="fa fa-lg fa-minus-square"></i></label>\
-        <div class="col-md-5 no-padding">\
-          <select id="select" class="form-control">\
-            <option>None</option>\
-            <option>2</option>\
-            <option>3</option>\
-            <option>4</option>\
-            <option>5</option>\
-          </select>\
-        </div>\
-        <div class="col-md-5 no-padding">\
-          <select id="select" class="form-control">\
-            <option>None</option>\
-            <option>2</option>\
-            <option>3</option>\
-            <option>4</option>\
-            <option>5</option>\
-          </select>\
-        </div>\
-        <div class="col-md-1"></div>\
-      </div>');
+          <label for="select" class="col-md-1 control-label"><i class="fa fa-lg fa-minus-square"></i></label>\
+          <div class="col-md-5 no-padding">\
+            <select id="pokeType" class="form-control">\
+              <option value="None">Type</option>\
+              <option value="Fire">Fire</option>\
+              <option value="Water">Water</option>\
+              <option value="Ice">Ice</option>\
+              <option value="Physics">Physics</option>\
+              <option value="Grass">Grass</option>\
+            </select>\
+          </div>\
+          <div class="col-md-5 no-padding">\
+            <select id="pokemonSelect" class="col-sm-3 form-control"></select>\
+          </div>\
+          <div class="col-md-1"></div>\
+        </div>');
   })
 
   $('.selection-form').on('click', function(e){
