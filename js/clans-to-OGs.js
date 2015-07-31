7f2bf77fa080eaccf51683a42e7259e3e7e3cc86
yschong1759 <yschong1759@gmail.com>
2015-07-31 19:22:35 +0800

    done cover-page

---
+++ js/clans-to-OGs.js
@@ -0,0 +1,27 @@
+$(".clans").on('click', function(e){
+    console.log(".clans is clicked");
+    console.log('data-bind value:' + $(this).attr("data-bind"));
+    
+    var to_be_rendered = $(this).attr("data-bind");
+    
+    var clans = {
+        'moltres': ['Charizard', 'Typhlosion', 'Blaziken', 'Infernape', 'Emboar'],
+        'darkrai': ['Honchkrow', 'Tyranitar', 'Sharpedo', 'Houndoom', 'Malamar'],
+        'kyurem': ['Glalie', 'Walrein', 'Beartic', 'Weavile', 'Aurorus'],
+        'lugia': ['Alakazam','Hypno', 'Slowking', 'Gallade', 'Metagross'],
+        'virizion': ['Venasaur', 'Meganium', 'Sceptile', 'Torterra', 'Serperior'],
+        'suicune': ['Blastoise', 'Feraligatr', 'Swampert', 'Empoleon', 'Samurott'] };
+     
+    e.stopPropagation();
+    e.preventDefault();
+
+    var matched = clans[to_be_rendered] || [];
+    console.log(matched);
+    var renderHtml = $.map(matched, function(matched){
+        return '<div class="col-md-2"> <img src="img/' + matched + '.jpg" class="pc-only img-responsive center-block"> \
+        <div style="background-color:Blue" class="img-div-bottom text-center zero-padding pc-only">' + matched + '</div><img src="img/'+ matched +'.png" class="mobile-only center-block"> \
+        <div style="background-color:Blue" class="img-div-onmobile text-center mobile-only">' + matched + '</div> \
+      </div>'}).join();
+    $(".clan2OG").html('<div class="col-md-1"></div>' + renderHtml + '<div class="col-md-1"></div>'); 
+})
+
