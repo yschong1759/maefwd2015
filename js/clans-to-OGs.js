88a6f2cb9c0d4590d0a6d20792705ea46a008496
yschong1759 <yschong1759@gmail.com>
2015-08-02 01:27:48 +0800

    OG ranking js done

--- js/clans-to-OGs.js
+++ js/clans-to-OGs.js
@@ -3,10 +3,57 @@ function backButton(){
     if (!($('.clan2OG').is(':hidden'))) {
         $('.clans').show();
         $('.clan2OG').hide();
+        window.scrollTo(0,0);
     } else if (!($('.final').is(':hidden'))) {
         $('.clan2OG').show();
         $('.final').hide();
+        window.scrollTo(0,0);
     };
+}
+
+//function to count ranking for each group
+function rankingCount(group) {
+    var Pokemon = Parse.Object.extend("pokemon");
+    var query = new Parse.Query(Pokemon);
+    var name = 'name';
+    query.find({
+        success: function(name) {
+            console.log("Successfully retrieved " + name.length + " scores.");
+            // Do something with the returned Parse.Object values
+            var dict = {};
+            console.log(dict);
+            for (var i = 0; i < name.length; i++) {
+              var object = name[i];
+              var target = object.get('experience');
+              
+              if (target in dict) {
+                dict[target] = dict[target] + ',' + object.get('name');
+              } else {
+                dict[target] = object.get('name') ;
+              }
+            };
+
+            //console.log(dict);
+
+            var rankingList = [];
+
+            var temp1 = Object.keys(dict);
+            var temp2 = [];
+            for (var i=0; i<temp1.length; i++) {
+              temp2[i] = parseInt(temp1[i]);
+            }
+
+            temp3 = temp2.reverse();
+
+            for(var i=0; i<temp3.length; i++) {
+              rankingList = rankingList.concat(dict[temp3[i]].split(','));
+              $('#Ranking').html(rankingList.indexOf(group) + 1);
+            }
+      },
+        error: function(query, error){
+            console.log(error);
+        }
+    });
 }
 
 //linking to OGs in each clan
@@ -45,6 +92,8 @@ $(".clans").on('click', function(e){
         $('.clan2OG').hide();
         $('.final').show();
 
+        window.scrollTo(0,0);
+
         var Pokemon = Parse.Object.extend("pokemon");
         var query = new Parse.Query(Pokemon);
 
@@ -63,6 +112,8 @@ $(".clans").on('click', function(e){
                 //P for public page
                 $('#ExperienceP').html(result["experience"]);
                 $('#CoinP').html(result["coin"]);
+                var temp = rankingCount(result["name"]);
+                $('#Ranking').html(temp);
 
             },
             error: function(error) {
